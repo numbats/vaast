2fbe9d37296c65e0521b588d6c144f48a3395a59
Harriet <harriet.mason@monash.edu>
2021-03-23 16:35:35 +1100

    fixed monotonic

--- R/association.R
+++ R/association.R
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 #' Measure of Spearman Correlation
 #'
 #' @param x numeric vector
 #' @param y numeric vector
 #'
 #' @return double
+#' @examples
 #' @export
 sc_monotonic <- function(x, y){
   cor(x, y, method='spearman')
 }
-=======
-# association measures go here
->>>>>>> 25f9207bf6d363ae2ca5dfaab00752e72cb67df7
