dea3f5716a0b5dd824fd2501345ce858b2213f88
Harriet <harriet.mason@monash.edu>
2021-03-24 12:57:13 +1100

    anscombe test on monotonic

--- R/association.R
+++ R/association.R
@@ -2,9 +2,23 @@
 #'
 #' @param x numeric vector
 #' @param y numeric vector
-#'
 #' @return double
 #' @examples
+#'   require(ggplot2)
+#'   require(tidyr)
+#'   require(dplyr)
+#'   data(anscombe)
+#'   anscombe_tidy <- anscombe %>%
+#'   pivot_longer(cols = everything(),
+#'     names_to = c(".value", "set"),
+#'     names_pattern = "(.)(.)")
+#'   ggplot(anscombe_tidy, aes(x=x, y=y)) +
+#'     geom_point() +
+#'     facet_wrap(~set, ncol=2, scales = "free")
+#'   sc_monotonic(anscombe$x1, anscombe$y1)
+#'   sc_monotonic(anscombe$x2, anscombe$y2)
+#'   sc_monotonic(anscombe$x3, anscombe$y3)
+#'   sc_monotonic(anscombe$x4, anscombe$y4)
 #' @export
 sc_monotonic <- function(x, y){
   cor(x, y, method='spearman')
