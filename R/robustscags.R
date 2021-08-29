cc708cdc14bdedb7e4b138bad21c32f62024fe3a
harriet-mason <harriet.mason@monash.edu>
2021-08-29 18:29:00 +1000

    initiliased clumpy robust scag

--- R/robustscags.R
+++ R/robustscags.R
@@ -1,10 +1,20 @@
 
 
-clumpy_value <- function(clustmst){
-  #calculate w
+inside_cluster <- function(mst, sc){
+  #mst matrix
+  matlist <- twomstmat(mst,scr)#function for matrix version of mst
+  mstmat <- matlist$mat #mst matrix
+  mstmat_lt <- matlist$lowertri #mst lower triangular matrix
 
-  #remvoe outliers
+  #calculate w
+  edges <- mstmat_lt[which(mstmat>0)]
+  w <- psi(edges)
+
+  #remove outliers
+  outliers <- outlying_identify(mst, sc)
+
   #calculate clumpy values
+  value <- clumpy_value()
   #return ej, clumpy val, w
 }
 
